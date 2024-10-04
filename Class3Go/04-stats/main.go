e005f911ebac800d1cc4ae7b0ff397f14b4dc45e
JTGlez <jorjeluigi1@gmail.com>
2024-10-04 12:36:52 -0600

    refactor 04-stats functions

--- Class3Go/04-stats/main.go
+++ Class3Go/04-stats/main.go
@@ -13,45 +13,51 @@ const (
 	maximum = "maximum"
 )
 
-func operation(calcType string) (func(nums ...int) int, error) {
+func minFunc(nums ...float64) float64 {
+	if len(nums) == 0 {
+		return 0
+	}
+	minimum := nums[0]
+	for _, num := range nums {
+		if num < minimum {
+			minimum = num
+		}
+	}
+	return minimum
+}
+
+func maxFunc(nums ...float64) float64 {
+	if len(nums) == 0 {
+		return 0
+	}
+	maximum := nums[0]
+	for _, num := range nums {
+		if num > maximum {
+			maximum = num
+		}
+	}
+	return maximum
+}
+
+func averageFunc(nums ...float64) float64 {
+	if len(nums) == 0 {
+		return 0
+	}
+	sum := 0.0
+	for _, num := range nums {
+		sum += num
+	}
+	return sum / float64(len(nums))
+}
+
+func operation(calcType string) (func(nums ...float64) float64, error) {
 	switch calcType {
 	case minimum:
-		return func(nums ...int) int {
-			if len(nums) == 0 {
-				return 0
-			}
-			minimum := nums[0]
-			for _, num := range nums {
-				if num < minimum {
-					minimum = num
-				}
-			}
-			return minimum
-		}, nil
+		return minFunc, nil
 	case maximum:
-		return func(nums ...int) int {
-			if len(nums) == 0 {
-				return 0
-			}
-			maximum := nums[0]
-			for _, num := range nums {
-				if num > maximum {
-					maximum = num
-				}
-			}
-			return maximum
-		}, nil
+		return maxFunc, nil
 	case average:
-		return func(nums ...int) int {
-			if len(nums) == 0 {
-				return 0
-			}
-			sum := 0
-			for _, num := range nums {
-				sum += num
-			}
-			return sum / len(nums)
-		}, nil
+		return averageFunc, nil
 	default:
 		return nil, errors.New("calculation type not defined")
 	}
@@ -76,14 +82,16 @@ func main() {
 		return
 	}
 
+	fmt.Println("Write a sequence of values. Then, press enter to calculate min, max and avg.")
+
 	args := helpers.ReadArgsFromStdin()
 	if len(args) == 0 {
 		panic("No args provided")
 	}
 
-	var values []int
+	var values []float64
 	for _, arg := range args {
-		value, err := strconv.Atoi(arg)
+		value, err := strconv.ParseFloat(arg, 64)
 		if err != nil {
 			panic(err)
 		}
@@ -94,8 +102,7 @@ func main() {
 	averageValue := averageFunc(values...)
 	maxValue := maxFunc(values...)
 
-	fmt.Printf("Valor mínimo: %d\n", minValue)
-	fmt.Printf("Valor promedio: %d\n", averageValue)
-	fmt.Printf("Valor máximo: %d\n", maxValue)
-
+	fmt.Printf("Valor mínimo: %.2f\n", minValue)
+	fmt.Printf("Valor promedio: %.2f\n", averageValue)
+	fmt.Printf("Valor máximo: %.2f\n", maxValue)
 }
